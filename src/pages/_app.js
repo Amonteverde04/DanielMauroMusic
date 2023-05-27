a43cb22c8e306850f168d4a358434dea43be088e
Alex Monteverde <alexmo@avi.com>
2023-05-27 19:21:34 -0400

    Attempting to fix missing icons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,9 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
   return (
